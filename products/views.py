8608bfa6de18849bb70aad7cb56c958b71772386
padmakumargs <padmakumargs@am.amrita.edu>
2023-08-04 10:27:12 +0530

    add pickle

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from .models import OrderedItems
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,5 +12,20 @@ def index(request):
     # return  HttpResponse("<h1>Welcome</h1>");
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>");
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def add_cart(request):
+    if request.method=='POST':
+        if request.POST.get('quantity'):
+            savequantity = OrderedItems()
+            savequantity.quantity = request.POST.get('quantity')
+            savequantity.save()
+
+            # savequantity = request.POST.get('quantity')
+            # savequantity.save()
+            messages.success(request, "Saved successfully")
+            return  render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
 
